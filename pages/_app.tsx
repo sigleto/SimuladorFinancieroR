23aaa57d80ddd2d23fd93869fab472905b3dcda9
sigleto <sigleto@gmail.com>
2025-01-12 16:54:57 +0100

    cambio para favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 // _app.tsx
 import '../Estilos/Global.css'
-
-import type { AppProps } from 'next/app';
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
