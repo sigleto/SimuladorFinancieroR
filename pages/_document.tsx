2229728389dec10c01d0b89871b154ef6a01c02c
sigleto <sigleto@gmail.com>
2025-01-14 17:45:49 +0100

    vertea

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,16 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="es">
-      <Head>
-        <meta name="description" content="Herramientas avanzadas para optimizar tus decisiones financieras. Simuladores de préstamos, ahorros, inversiones y más." />
-        <meta property="og:title" content="Simuladores Financieros" />
-        <meta property="og:description" content="Optimiza tus finanzas con nuestros simuladores avanzados." />
-        <meta property="og:image" content="/logo.png" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png"></link>
-      </Head>
+     <Head>
+  <title>Simuladores Financieros</title>
+  <meta name="description" content="Herramientas avanzadas para optimizar tus decisiones financieras. Simuladores de préstamos, ahorros, inversiones y más." />
+  <meta property="og:title" content="Simuladores Financieros" />
+  <meta property="og:description" content="Optimiza tus finanzas con nuestros simuladores avanzados." />
+  <meta property="og:image" content="/logo.png" />
+  <link rel="icon" href="/favicon.ico" />
+  <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+</Head>
+
       <body>
         <Main />
         <NextScript />
