0d59eafb85ca87a6af74847822e95ff53fed60ec
sigleto <sigleto@gmail.com>
2025-01-20 16:47:06 +0100

    anuncios

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,25 @@
-// _document.tsx
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="es">
-     <Head>
-  <title>Simuladores Financieros</title>
-  <meta name="description" content="Herramientas avanzadas para optimizar tus decisiones financieras. Simuladores de préstamos, ahorros, inversiones y más." />
-  <meta property="og:title" content="Simuladores Financieros" />
-  <meta property="og:description" content="Optimiza tus finanzas con nuestros simuladores avanzados." />
-  <meta property="og:image" content="/logo.png" />
-  <link rel="icon" href="/favicon.ico" />
-  <link rel="icon" type="image/png" sizes="48x48" href="/favicon.png" />
-  <link rel="canonical" href="https://simuladorfinanciero.com/" />
-</Head>
-
+      <Head>
+        <title>Simuladores Financieros</title>
+        <meta name="description" content="Herramientas avanzadas para optimizar tus decisiones financieras. Simuladores de préstamos, ahorros, inversiones y más." />
+        <meta property="og:title" content="Simuladores Financieros" />
+        <meta property="og:description" content="Optimiza tus finanzas con nuestros simuladores avanzados." />
+        <meta property="og:image" content="/logo.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon.png" />
+        <link rel="canonical" href="https://simuladorfinanciero.com/" />
+        
+        {/* Código de Google AdSense */}
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6921150380725872" 
+          crossOrigin="anonymous">
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
